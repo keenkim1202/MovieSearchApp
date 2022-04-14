370164c4cee20f44242d126720b2e0d96d0e8722
KEEN <homurahomus@gmail.com>
2022-04-15 02:20:37 +0900

    [Feat] SearchView 적용

--- MovieSearchApp/Sources/Controller/SearchViewController.swift
+++ MovieSearchApp/Sources/Controller/SearchViewController.swift
@@ -9,10 +9,56 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+  // MARK: Properties
+  let searchView = SearchView()
+  
+  // MARK: View Life-Cycle
+  override func loadView() {
+    self.view = searchView
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .white
+    configure()
   }
-
+  
+  // MARK: Configure
+  func configure() {
+    configureNavigationBar()
+    
+    searchView.tableView.delegate = self
+    searchView.tableView.dataSource = self
+  }
+  
+  func configureNavigationBar() {
+    title = "영화검색"
+    self.navigationController?.navigationBar.prefersLargeTitles = true
+    
+    let rightBarButton = UIBarButtonItem(title: "즐겨찾기 목록", style: .done, target: self, action: #selector(onFavoriteList))
+    self.navigationItem.rightBarButtonItem = rightBarButton
+  }
+  
+  
+  @objc func onFavoriteList() {
+    print(#function)
+  }
+  
 }
 
+// MARK: Extensions
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 5 // test
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchTableViewCell.identifier, for: indexPath) as? SearchTableViewCell else { return UITableViewCell() }
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 110 // test
+  }
+  
+  
+}
