3ece467afb3b96a91b5c5fe7fcb869b9d2d5b0c7
KEEN <homurahomus@gmail.com>
2022-04-15 02:19:56 +0900

    [Feat] SearchViewController의 custom UI 구성

--- MovieSearchApp/Sources/View/SearchTableViewCell.swift
+++ MovieSearchApp/Sources/View/SearchTableViewCell.swift
@@ -6,18 +6,114 @@
 //
 
 import UIKit
+import SnapKit
 
-class SearchTableViewCell: UITableViewCell {
+class SearchTableViewCell: UITableViewCell, ViewRepresentable {
+  
+  static let identifier = String(describing: SearchTableViewCell.self)
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+  // MARK: - UI
+  let posterImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(systemName: "square.slash")
+    imageView.contentMode = .scaleAspectFit
+    imageView.backgroundColor = .mainOrangeColor
+    imageView.tintColor = .white
+    return imageView
+  }()
+  
+  let verticalStackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    stackView.spacing = 5
+    return stackView
+  }()
+  
+  let titleLabel: UILabel = {
+    let label = UILabel()
+    label.text = "title"
+    label.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+    return label
+  }()
+  
+  let directorLabel: UILabel = {
+    let label = UILabel()
+    label.text = "director"
+    label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+    return label
+  }()
+  
+  let actorLabel: UILabel = {
+    let label = UILabel()
+    label.text = "actor"
+    label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+    return label
+  }()
+  
+  let rateLabel: UILabel = {
+    let label = UILabel()
+    label.text = "rate"
+    label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+    return label
+  }()
+  
+  let starButton: UIButton = {
+    let button = UIButton()
+    button.setImage(UIImage(systemName: "star"), for: .normal)
+    button.tintColor = .mainOrangeColor
+    return button
+  }()
+  
+  // MARK: - Init
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    createViews()
+    setConstraints()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Configure
+  func createViews() {
+    [titleLabel, directorLabel, actorLabel, rateLabel].forEach {
+      verticalStackView.addArrangedSubview($0)
+    }
+    
+    [posterImageView, verticalStackView, starButton].forEach {
+      contentView.addSubview($0)
+    }
+  }
+  
+  func setConstraints() {
+    [posterImageView, verticalStackView, starButton, titleLabel, directorLabel, actorLabel, rateLabel].forEach {
+      $0.translatesAutoresizingMaskIntoConstraints = false
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    let safeArea = contentView.safeAreaLayoutGuide
+    let buttonWidth: CGFloat = 20
+    
+    posterImageView.snp.makeConstraints {
+      $0.width.equalTo(posterImageView.snp.height).multipliedBy(0.7)
+      $0.leading.equalTo(safeArea).offset(10)
+      $0.top.equalTo(safeArea).offset(10)
+      $0.bottom.equalTo(safeArea).offset(-10)
     }
-
+    
+    verticalStackView.snp.makeConstraints {
+      $0.leading.equalTo(posterImageView.snp.trailing).offset(10)
+      $0.top.equalTo(safeArea).offset(10)
+      $0.bottom.equalTo(safeArea).offset(-10)
+      $0.trailing.equalTo(safeArea).offset(-10)
+    }
+    
+    starButton.snp.makeConstraints {
+      $0.width.height.equalTo(buttonWidth)
+      $0.trailing.equalTo(safeArea).offset(-10)
+      $0.top.equalTo(safeArea).offset(10)
+    }
+    
+  }
+  
 }
--- MovieSearchApp/Sources/View/SearchView.swift
+++ MovieSearchApp/Sources/View/SearchView.swift
@@ -5,4 +5,45 @@
 //  Created by KEEN on 2022/04/15.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class SearchView: UIView, ViewRepresentable {
+  
+  // MARK: - UI
+  let tableView: UITableView = {
+    let tableView = UITableView()
+    return tableView
+  }()
+  
+  // MARK: - Init
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    registerCells()
+    createViews()
+    setConstraints()
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Configure
+  func registerCells() {
+    tableView.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.identifier)
+  }
+  
+  func createViews() {
+    addSubview(tableView)
+  }
+  
+  func setConstraints() {
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    
+    tableView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+  }
+  
+}
