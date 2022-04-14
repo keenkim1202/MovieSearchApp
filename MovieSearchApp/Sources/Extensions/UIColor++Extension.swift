2e5eaaba59bc10fa3057ead1391217b0e8d4d038
KEEN <homurahomus@gmail.com>
2022-04-15 02:18:58 +0900

    [Add] cutom color extension 추가

---
+++ MovieSearchApp/Sources/Extensions/UIColor++Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor++Extension.swift
+//  MovieSearchApp
+//
+//  Created by KEEN on 2022/04/15.
+//
+
+import UIKit
+
+extension UIColor {
+  public class var mainOrangeColor: UIColor {
+    return UIColor(named: "mainOrangeColor")!
+  }
+}
